6257c6d35552841fb54d847397194e49db924c5c
Eren Yeager <154471897+SquirtleRivals09@users.noreply.github.com>
2024-02-06 11:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -85,14 +85,14 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/2faeed093fed5082bacf3.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/13f958005cec4578b862e.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/13f958005cec4578b862e.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/ff4af5d2148b426de1d6e.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/13f958005cec4578b862e.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/13f958005cec4578b862e.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/13f958005cec4578b862e.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/13f958005cec4578b862e.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6e73f1a3ca224c1babc88.jpg"
 
 
 def time_to_seconds(time):
